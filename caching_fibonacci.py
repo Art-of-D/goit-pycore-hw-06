b12df5be574bbbb5bae39a06a78c4c2c1724a2cb
Artur Didur <didur.art@gmail.com>
2024-12-20 21:51:09 +0100

    fix fib

--- caching_fibonacci.py
+++ caching_fibonacci.py
@@ -1,14 +1,24 @@
-def caching_fibonacci(n, cache):
-  if n in cache:
-    return cache[n]
-  else:
-    result = caching_fibonacci(n - 1, cache) + caching_fibonacci(n - 2, cache)
-    cache[n] = result
-    return result
-  
-# Отримуємо функцію fibonacci
+from typing import Callable
+
+def caching_fibonacci() -> Callable[[], int]:
+  """
+  Returns a function that calculates the nth Fibonacci number, but caches
+  previously calculated numbers to speed up the calculation.
+  """
+  cache = {}
+  def fibonacci(n) -> int:
+    if n <= 1:
+      return n
+    if n in cache:
+      return cache[n]
+    else:
+      result = fibonacci(n - 1) + fibonacci(n - 2)
+      cache[n] = result
+      return result
+
+  return fibonacci
+
 fib = caching_fibonacci()
 
-# Використовуємо функцію fibonacci для обчислення чисел Фібоначчі
 print(fib(10))  # Виведе 55
 print(fib(15))  # Виведе 610
