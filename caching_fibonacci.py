2ed81602df55fab68ac73e866bb039106f30b6cf
Artur Didur <didur.art@gmail.com>
2024-12-21 13:19:31 +0100

    fix generator numbers

--- caching_fibonacci.py
+++ caching_fibonacci.py
@@ -4,6 +4,8 @@ def caching_fibonacci() -> Callable[[], int]:
   """
   Returns a function that calculates the nth Fibonacci number, but caches
   previously calculated numbers to speed up the calculation.
+
+  Need to use through function variable, like fib = caching_fibonacci()
   """
   cache = {}
   def fibonacci(n) -> int:
@@ -18,7 +20,9 @@ def caching_fibonacci() -> Callable[[], int]:
 
   return fibonacci
 
+# Getting the function
 fib = caching_fibonacci()
 
+# Using the function
 print(fib(10))  # Виведе 55
 print(fib(15))  # Виведе 610
--- generator_numbers.py
+++ generator_numbers.py
@@ -1,8 +1,45 @@
-def generator_numbers():
-  num = 1
-  while True:
-    yield num
-    num += 1
+from decimal import Decimal
+
+def generator_numbers(text):
+  """
+  Extracts numbers from a given text and yields them as Decimal objects.
+
+  Args:
+      text (str): The input string containing numbers and other text.
+
+  Yields:
+      Decimal: Each number found in the text, converted to a Decimal object.
+
+  Raises:
+      ValueError: If the conversion of a word to a float fails.
+  """
+
+  if not text:
+      return
+  list = text.split()
+  for word in list:
+      try:
+          float(word)
+          num = Decimal(word)
+          yield num
+      except ValueError:
+          continue
+
+def sum_profit(text, generator_numbers):
+  """
+  Sums all the numbers in a given text.
+
+  Args:
+      text (str): The input string containing numbers and other text.
+      generator_numbers (function): A generator of Decimal objects from a given text.
+
+  Returns:
+      Decimal: The sum of all the numbers in the text.
+  """
+  total = Decimal(0)
+  for num in generator_numbers(text):
+    total += Decimal(num)
+  return total
 
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 total_income = sum_profit(text, generator_numbers)
